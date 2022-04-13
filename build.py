b16c2c3b2b6900eaf564d42bc9fbc382489e68c3
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2022-04-13 18:03:12 +0200

    Renamed public to docs for GH pages

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from staticjinja import Site
 
 
 if __name__ == "__main__":
-    site = Site.make_site(outpath="public", extensions=['jinja_markdown.MarkdownExtension'])
+    site = Site.make_site(outpath="docs", extensions=['jinja_markdown.MarkdownExtension'])
     # enable automatic reloading
     site.render(use_reloader=True)
